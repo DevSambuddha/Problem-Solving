744a3db88f8f2f3ab4b7038bd5b2e0a65d3c8aa7
Sambuddha Biswas <38996553+XiLucas@users.noreply.github.com>
2024-09-06 12:40:22 +0530

    1st Problem

---
+++ problem1.java
@@ -0,0 +1,24 @@
+//Print the pattern
+//* * * * 
+//* * * * 
+//* * * * 
+//* * * * 
+
+import java.util.*;
+
+public class problem1 {
+    public static void print1(int n){
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print1(n);
+        sc.close();
+    }
+}
+
