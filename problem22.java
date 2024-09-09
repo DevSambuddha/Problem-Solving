06ca9cde8271c27cafeeb90554d932ec8c47e47e
Sambuddha Biswas <38996553+XiLucas@users.noreply.github.com>
2024-09-09 13:42:57 +0530

    22nd Problem

---
+++ problem22.java
@@ -0,0 +1,32 @@
+// Print the pattern for n=5
+/*          
+*****
+*   *
+*   *
+*   *
+*****
+ */
+
+ import java.util.*;
+ import static java.lang.Math.min; // Import the min function
+ 
+ public class problem22 {
+     public static void print22(int n) {
+        for (int i=0;i<2*n-1;i++) {
+                for(int j=0;j<2*n-1;j++) {
+                int top = i;
+                int left = j;
+                int right = (2*n-2)-j;
+                int down = (2*n-2)-i; 
+                System.out.print(n - min(min(top,down), min(left,right)));
+            }System.out.println();
+        }
+        }
+    
+     public static void main(String args[]){
+         Scanner sc = new Scanner (System.in);
+         int n = sc.nextInt();
+         print22(n);
+         sc.close();
+     }
+ }
